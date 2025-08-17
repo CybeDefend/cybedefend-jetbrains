ae378e2be25ec01688767990368fc5eb168842c7
JulienZammit <78787470+JulienZammit@users.noreply.github.com>
2025-08-18 00:20:18 +0200

    fix: build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,8 +56,3 @@ tasks {
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> { kotlinOptions.jvmTarget = "21" }
 }
-        sourceCompatibility = "21"
-        targetCompatibility = "21"
-    }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> { kotlinOptions.jvmTarget = "21" }
-}
